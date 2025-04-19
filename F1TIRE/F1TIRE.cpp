d79f50ab4add3cca80f40afd8d192e09e7dc7bff
MichalK252 <u39_mickar_lbn@technischools.com>
2025-04-19 14:01:00 +0200

    First function for choosing the race is completed

--- F1TIRE/F1TIRE.cpp
+++ F1TIRE/F1TIRE.cpp
@@ -1,20 +1,52 @@
-﻿// F1TIRE.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <iomanip>
+using namespace std;
 
-#include <iostream>
+void showRaces(const vector<std::string>& races) 
+{
+    cout << R"(
+ ________  _____          _________  ___  ________  _______      
+|\  _____\/ __  \        |\___   ___\\  \|\   __  \|\  ___ \     
+\ \  \__/|\/_|\  \       \|___ \  \_\ \  \ \  \|\  \ \   __/|    
+ \ \   __\|/ \ \  \           \ \  \ \ \  \ \   _  _\ \  \_|/__  
+  \ \  \_|    \ \  \           \ \  \ \ \  \ \  \\  \\ \  \_|\ \ 
+   \ \__\      \ \__\           \ \__\ \ \__\ \__\\ _\\ \_______\
+    \|__|       \|__|            \|__|  \|__|\|__|\|__|\|_______|
+)" << endl;
+
+    for (size_t i = 0; i < races.size(); ++i) {
+        cout << setw(2) << i + 1 << ". " << races[i] << "\n";
+    }
+}
+
+int getRaceSelection(int max) {
+    int choice;
+    do {
+        cout << "Enter a race number: ";
+        cin >> choice;
+    } while (choice < 1 || choice > max);
+    return choice - 1;
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    vector<string> races = {
+        "F1 Australian GP", "F1 Chinese GP", "F1 Japanese GP", "F1 Bahrain GP", "F1 Saudi Arabian GP", "F1 Miami GP",
+        "F1 Emilia-Romagna GP", "F1 Monaco GP", "F1 Spain GP", "F1 Canada GP", "F1 Austria GP", "F1 Great Britain",
+        "F1 Belgium GP", "F1 Hungary GP", "F1 Netherlands GP", "F1 Italy GP", "F1 Azerbaijan GP", "F1 Singapore GP",
+        "F1 United-States GP", "F1 Mexico GP", "F1 Brazil GP", "F1 Las-Vegas GP", "F1 Qatar GP", "F1 Abu-Dhabi GP",
+    };
+
+    showRaces(races);
+    int selectedRace = getRaceSelection(races.size());
+
+    cout << "\nWybrano wyścig: " << races[selectedRace] << "\n";
+
+    string currentRace = races[selectedRace];
+
+    return 0;
+
 }
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
